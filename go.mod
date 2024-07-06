6f9115d1f8b14d46a199dfdddbd7939a48478ecd
utukj <utukphd@gmail.com>
2024-07-06 14:01:35 +0100

    first code commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/utukj/ipinfo-client
+
+go 1.19
+
+require (
+	github.com/ipinfo/go/v2 v2.10.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	golang.org/x/sync v0.0.0-20220513210516-0976fa681c29 // indirect
+)
---
+++ main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/ipinfo/go/v2/ipinfo"
+	"github.com/joho/godotenv"
+)
+
+type InfoResp struct {
+	ClientIP string `json:"client_ip"`
+	Location string `json:"location"`
+	Greeting string `json:"greeting"`
+}
+
+type WeatherDesc struct {
+	Main        string `json:"main"`
+	Description string `json:"description"`
+}
+
+type WeatherDeets struct {
+	Temp float32 `json:"temp"`
+}
+
+type OpenWeatherResp struct {
+	Weather []WeatherDesc `json:"weather"`
+	Main    WeatherDeets  `json:"main"`
+	Base    string        `json:"base"`
+	Dt      int           `json:"dt"`
+}
+
+func kevinToCelcius(f float32) float32 {
+	return f - 273
+}
+
+func main() {
+	fmt.Println("Running ipinfo-client!")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading env file: ", err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
+
+	http.HandleFunc("/getinfo", func(w http.ResponseWriter, r *http.Request) {
+
+		clientIP := r.RemoteAddr
+
+		ipInfoToken := os.Getenv("IPINFO_TOKEN")
+		client := ipinfo.NewClient(nil, nil, ipInfoToken)
+		ipinfo, err := client.GetIPInfo(net.ParseIP(clientIP))
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		visitorName := r.URL.Query().Get("visitor_name")
+		if visitorName == "" {
+			visitorName = "Nameless User"
+		}
+
+		openWeatherToken, ok := os.LookupEnv("OPENWEATHER_TOKEN")
+		if !ok {
+			log.Fatalln("could not find token")
+		}
+		openWeatherURL := fmt.Sprintf(
+			"https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s",
+			ipinfo.City,
+			openWeatherToken)
+
+		fmt.Println("Making request to: ", openWeatherURL)
+
+		resp, err := http.Get(openWeatherURL)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		decodedResp := OpenWeatherResp{}
+		json.NewDecoder(resp.Body).Decode(&decodedResp)
+
+		info := InfoResp{
+			ClientIP: r.RemoteAddr,
+			Location: ipinfo.City,
+			Greeting: fmt.Sprintf(
+				"Hi %s. The weather is %.2f degrees celcius in %s with %s.",
+				visitorName,
+				kevinToCelcius(decodedResp.Main.Temp),
+				ipinfo.City,
+				decodedResp.Weather[0].Description,
+			),
+		}
+
+		fmt.Println("Delivering getinfo response")
+		json.NewEncoder(w).Encode(info)
+	})
+
+	err = http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
